fc00967cf61048c15d7d5a7bbec69717c4def967
Peter Petrov <exeption11111@gmail.com>
2022-12-23 00:45:56 +0200

    dada

--- src/components/OverThreeHundred.jsx
+++ src/components/OverThreeHundred.jsx
@@ -4,12 +4,12 @@ import styles from '../styles'
 
 const Clients = () => {
   return (
-     <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-20`}>
+     <section className={`${styles.flexCenter} sm:flex-row flex-col flex-wrap sm:mb-20 mb-20`}>
         {stats.map((stat)=>(
           <div key={stat.id} className={`flex-1 flex justify-center items-center flex-row m-3 `}>
-                <h4 className='font-poppins font-semibold xs:text-[40px] md:text-[10px] lg:text-[45px] xs:leading-[53px] leading-[43px] text-dimWhite'>
+                <h4 className='font-poppins font-semibold text-[30px] sm:text-[45px] xs:leading-[53px] leading-[43px] text-dimWhite'>
                   {stat.value}</h4>
-                <p className='font-poppins font-bold tracing-widest sm:text-[25px] text-[14px] xs:leading-[26px] text-gradient leading-[21px] uppercase ml-3'>{stat.title} </p>
+                <p className='font-poppins font-bold tracing-widest sm:text-[25px] text-[20px] text-gradient uppercase ml-3'>{stat.title} </p>
           </div>
         ))}
      </section>
